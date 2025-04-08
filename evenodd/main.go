94de0d73c0abaf37b210bfb541d2f16cdada0d7c
Your Name <enzothomazi@hotmail.com>
2025-04-08 20:56:59 -0300

    feat: Refactor even odd project

--- evenodd/main.go
+++ evenodd/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	n := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for i := 0; i < len(n); i++ {
+	for i := range n {
 		if n[i]%2 == 0 {
 			fmt.Printf("%v is even\n", n[i])
 		} else {
