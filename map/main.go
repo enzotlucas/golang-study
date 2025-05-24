9c69ab643a6e36d3d5cf5add1d9ab89197ac305f
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2025-05-24 18:10:29 -0300

    feat: Adding maps

---
+++ map/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var colors map[string]string
+
+	// colors := make(map[string]string)
+
+	colors := map[string]string{
+		"red":   "#FF0000",
+		"green": "#008000",
+	}
+	colors["white"] = "#FFFFFF"
+
+	// delete(colors, "red") remove values from map
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
